4463bffcc71221096697bf7e1e604d3ab31d44e3
Maclenn77 <maclenn77@gmail.com>
2021-12-22 11:42:57 -0600

    Changes for working with buttons on the homepage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import _ from 'lodash';
 import './style.css';
-import GET from './pokemon.js';
+import get from './pokemon.js';
 
 const pokeHeader = document.querySelector('.modal-header');
+const pokeDetails = document.querySelectorAll('.details');
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-function openModal(modal) {
+// Modal Functions
+function openModal(modal, cardId) {
   if (modal == null) return
   modal.classList.add('active')
   overlay.classList.add('active')
-  displayDetails(3);
+  displayDetails(cardId);
 }
 
 function closeModal(modal) {
@@ -23,9 +25,10 @@ function closeModal(modal) {
 }
 
 openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (x) => {
       const modal = document.querySelector(button.dataset.modalTarget)
-      openModal(modal)
+      const cardId = x.target.parentElement.parentElement.id;
+      openModal(modal, cardId)
     })
   })
   
@@ -46,13 +49,11 @@ openModalButtons.forEach(button => {
 
 
 async function displayDetails(pokeID) {
-  let pokemon = await GET.aPokemon(pokeID);
-  const pokeHeader = document.querySelector('.modal-header');
-  const pokeDetails = document.querySelectorAll('span');
+  let pokemon = await get.aPokemon(pokeID);
   pokeHeader.insertAdjacentHTML('beforeend',`<h2 class='col-12'>${pokemon.name.toUpperCase()}, #${pokeID}</p>`);
   pokeHeader.insertAdjacentHTML('afterbegin', `<img src='${pokemon.image}' alt='A sprite of ${pokemon.name}' class='col-10'>`);
   pokemon = [pokemon.height, pokemon.weight, pokemon.types, pokemon.exp];
-  let i = -1;
+  let i = 0;
   pokeDetails.forEach(span => {
       span.innerHTML = pokemon[i];
       i++;
