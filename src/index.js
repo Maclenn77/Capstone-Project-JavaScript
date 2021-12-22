6d695266d1b9b858b6ed04ccfff9abe2bcac2ded
Maclenn77 <maclenn77@gmail.com>
2021-12-22 00:23:55 -0600

    Popup is working

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import './style.css';
 import GET from './pokemon.js';
 
-const pokeDetails = document.querySelector('.details');
+const pokeHeader = document.querySelector('.modal-header');
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
@@ -11,12 +11,15 @@ function openModal(modal) {
   if (modal == null) return
   modal.classList.add('active')
   overlay.classList.add('active')
+  displayDetails(3);
 }
 
 function closeModal(modal) {
   if (modal == null) return
   modal.classList.remove('active')
   overlay.classList.remove('active')
+  pokeHeader.removeChild(pokeHeader.firstChild);
+  pokeHeader.removeChild(pokeHeader.lastChild);
 }
 
 openModalButtons.forEach(button => {
@@ -42,15 +45,16 @@ openModalButtons.forEach(button => {
 
 
 
-// async function displayDetails(pokeID) {
-//   const pokemon = await GET.aPokemon(pokeID);
-//   pokeDetails.insertAdjacentHTML('beforeend', `<img src='${pokemon.image}' alt='A sprite of ${pokemon.name}' class='col-10'> <button data-close-button class="close-button col-2">&times;</button>`);
-//   pokeDetails.insertAdjacentHTML('beforeend', `<h2 class='col-12'>${pokemon.name.toUpperCase()}, #${pokeID}</p>`);
-//   pokeDetails.insertAdjacentHTML('beforeend', `<p class='col-6'>Height: ${pokemon.height}</p>`);
-//   pokeDetails.insertAdjacentHTML('beforeend', `<p class='col-6'>Weight: ${pokemon.weight}</p>`);
-//   pokeDetails.insertAdjacentHTML('beforeend', `<div class='w-100'></div>`);
-//   pokeDetails.insertAdjacentHTML('beforeend', `<p class='col-6'>Type: ${pokemon.types}</p>`);
-//   pokeDetails.insertAdjacentHTML('beforeend', `<p class='col-6'>Base Experience: ${pokemon.exp}</p>`);
-// }
-
-// displayDetails(679);
+async function displayDetails(pokeID) {
+  let pokemon = await GET.aPokemon(pokeID);
+  const pokeHeader = document.querySelector('.modal-header');
+  const pokeDetails = document.querySelectorAll('span');
+  pokeHeader.insertAdjacentHTML('beforeend',`<h2 class='col-12'>${pokemon.name.toUpperCase()}, #${pokeID}</p>`);
+  pokeHeader.insertAdjacentHTML('afterbegin', `<img src='${pokemon.image}' alt='A sprite of ${pokemon.name}' class='col-10'>`);
+  pokemon = [pokemon.height, pokemon.weight, pokemon.types, pokemon.exp];
+  let i = -1;
+  pokeDetails.forEach(span => {
+      span.innerHTML = pokemon[i];
+      i++;
+  });
+}
