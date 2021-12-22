bd51f4dfe6d339e77934fc49809ee0222874bf08
ShadyShawkat <shadyshawkat91@gmail.com>
2021-12-22 18:46:58 +0200

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import _ from "lodash";
-import "./style.css";
-import pokeAPI from "./pokeAPIHandler";
+import './style.css';
+import pokeAPI from './pokeAPIHandler.js';
 
-const pokemonCardsSection = document.querySelector(".pokemon-cards");
-const pageLinks = document.querySelectorAll(".page-link");
+const pokemonCardsSection = document.querySelector('.pokemon-cards');
+const pageLinks = document.querySelectorAll('.page-link');
 let pageNum = 1;
 const pokemonsPerPage = 12;
 
 const createCard = (pokemonId, pokemon) => {
-  const cardDiv = document.createElement("div");
-  cardDiv.classList = "card col-md-3 d-flex flex-column overflow-hidden";
+  const cardDiv = document.createElement('div');
+  cardDiv.classList = 'card col-md-3 d-flex flex-column overflow-hidden';
   cardDiv.id = pokemonId;
   cardDiv.innerHTML = `
   <button class="position-absolute bg-dark rounded-circle heart-btn">
@@ -17,8 +16,8 @@ const createCard = (pokemonId, pokemon) => {
     <i class="far fa-heart"></i>
   </button>
   <img src="${pokeAPI.apiSpritesURL(pokemonId)}" class="card-img-top" alt="${
-    pokemon.name
-  }'s image">
+  pokemon.name
+}'s image">
   <div class="card-body text-center mt-auto">
       <h4 class="card-title fs-4">${pokemon.name}</h4>
       <a href="#" class="btn btn-outline-primary">Comments</a>
@@ -28,64 +27,67 @@ const createCard = (pokemonId, pokemon) => {
 };
 
 const changeLinks = (currentPage) => {
-  const pageNumbersElements = [...pageLinks].filter(link => !link.classList.contains('next-page') && !link.classList.contains('previous-page'));
-  pageNumbersElements.forEach(link => link.parentNode.classList.remove('active'));
+  const pageNumbersElements = [...pageLinks].filter((link) => !link.classList.contains('next-page') && !link.classList.contains('previous-page'));
+  pageNumbersElements.forEach((link) => link.parentNode.classList.remove('active'));
   currentPage = parseInt(currentPage, 10);
   const pageArr = [];
-  for (let i = currentPage - 3; i < currentPage + 4; i+=1) {
+  for (let i = currentPage - 3; i < currentPage + 4; i += 1) {
     pageArr.push(i);
   }
-  if(pageArr[0] < 1) {
-    pageArr.forEach((_, index) => {pageArr[index] = index+1});
+  if (pageArr[0] < 1) {
+    pageArr.forEach((_, index) => { pageArr[index] = index + 1; });
   }
-  if(pageArr[pageArr.length-1] > 94) {
+  if (pageArr[pageArr.length - 1] > 94) {
     const firstPageNum = 88;
-    pageArr.forEach((_, index) => {pageArr[index] = firstPageNum+index});
+    pageArr.forEach((_, index) => { pageArr[index] = firstPageNum + index; });
   }
-  if(pageArr[0] > 1) {
+  if (pageArr[0] > 1) {
     pageArr[0] = '...';
   }
-  if(pageArr[pageArr.length-1] < 94) {
-    pageArr[pageArr.length-1] = '...';
+  if (pageArr[pageArr.length - 1] < 94) {
+    pageArr[pageArr.length - 1] = '...';
   }
-  pageNumbersElements.forEach((link, index) => {link.textContent = pageArr[index]})
-  const currentPageElement = pageNumbersElements.find(link => parseInt(link.textContent, 10) === currentPage);
+  pageNumbersElements.forEach((link, index) => { link.textContent = pageArr[index]; });
+  const currentPageElement = pageNumbersElements.find((link) => {
+    link = parseInt(link.textContent, 10);
+    return link === currentPage;
+  });
   currentPageElement.parentNode.classList.add('active');
-}
+};
 
 const renderUI = (pageNumber) => {
   pokemonCardsSection.innerHTML = '';
   pokeAPI.fetchPokemonsData(pageNumber).then((data) => {
     data.results.forEach((pokemon, index) => {
       let pokemonId = index + ((pageNumber - 1) * pokemonsPerPage) + 1;
-      if(pokemonId>=899) {
-        pokemonId+=9102;
+      if (pokemonId >= 899) {
+        pokemonId += 9102;
       }
       const card = createCard(pokemonId, pokemon);
       pokemonCardsSection.appendChild(card);
     });
   });
   changeLinks(pageNumber);
-}
+};
 
 // Pagination
-pageLinks.forEach((pageLink)=> {
-  pageLink.addEventListener("click", (e)=> {
+pageLinks.forEach((pageLink) => {
+  pageLink.addEventListener('click', (e) => {
     let pageChanged = false;
-    if(e.target.textContent === "Next" && pageNum < 94) {
+    if (e.target.textContent === 'Next' && pageNum < 94) {
       pageNum += 1;
       pageChanged = true;
-    } else if(e.target.textContent === "Previous" && pageNum > 1) {
+    } else if (e.target.textContent === 'Previous' && pageNum > 1) {
       pageNum -= 1;
       pageChanged = true;
-    } else if(e.target.textContent !== "..." && e.target.textContent !== "Next" && e.target.textContent !== "Previous") {
+    } else if (e.target.textContent !== '...' && e.target.textContent !== 'Next' && e.target.textContent !== 'Previous') {
       pageNum = parseInt(e.target.textContent, 10);
       pageChanged = true;
     }
-    if(pageChanged) {
+    if (pageChanged) {
       renderUI(pageNum);
     }
-  })
-})
+  });
+});
 
 renderUI(pageNum);
--- src/pokeAPIHandler.js
+++ src/pokeAPIHandler.js
@@ -1,13 +1,11 @@
 const pokemonsPerPage = 12;
-const apiURL = "https://pokeapi.co/api/v2/pokemon/";
-const apiSpritesURL = (id) =>
-  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+const apiURL = 'https://pokeapi.co/api/v2/pokemon/';
+const apiSpritesURL = (id) => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
-const fetchPokemonsData = (pageNum) =>
-  fetch(
-    `${apiURL}?limit=${pokemonsPerPage}&offset=${
-      (pageNum - 1) * pokemonsPerPage
-    }`
-  ).then((data) => data.json());
+const fetchPokemonsData = (pageNum) => fetch(
+  `${apiURL}?limit=${pokemonsPerPage}&offset=${
+    (pageNum - 1) * pokemonsPerPage
+  }`,
+).then((data) => data.json());
 
 export default { fetchPokemonsData, apiSpritesURL };
